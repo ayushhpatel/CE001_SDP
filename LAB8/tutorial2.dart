0f956c7dac527aeb8a5371e6a99da8ab67bff1af
ayushhpatel <84513508+ayushhpatel@users.noreply.github.com>
2022-09-19 23:11:28 +0530

    Create tutorial2.dart

---
+++ LAB8/tutorial2.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+//import 'package:lab8_2/quote.dart';
+import 'quote.dart'; // due to same directory file no need path
+void main() => runApp(MaterialApp(
+  home: EchoList(),
+));
+class EchoList extends StatefulWidget {
+  const EchoList({Key? key}) : super(key: key);
+  @override
+  State<EchoList> createState() => _EchoListState();
+}
+class _EchoListState extends State<EchoList> {
+/*
+List<String> quotes = [
+'The truth is realy pure and never simple',
+'I see humans but no humanity',
+'The time is always right to do what is right'
+];
+// List<String> author = [];
+*/
+/* this will create error..because list is now not of string....
+List<String> quotes = [
+Quote(text: 'The truth is realy pure and never simple',author:
+'jignesh1'),
+];
+*/
+  List<Quote> quotes = [
+    Quote(text: 'The truth is realy pure and never simple',author:
+    'ayush1'),
+    Quote(author: 'ayush2', text: 'I see humans but no humanity'),
+    Quote(text: 'The time is always right to do what is right',author:
+    'ayush1'),
+  ];
+  Widget quoteTemplate(quote){
+    return Card(
+      margin: EdgeInsets.fromLTRB(20.0, 30.0, 40.0, 10.0),
+      child: Padding(
+        padding: const EdgeInsets.all(12.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(
+              quote.text,
+              style: TextStyle(
+                fontSize: 20,
+                color: Colors.deepPurple,
+              ),
+            ),
+            SizedBox(height: 10),
+            Text(
+              quote.author,
+              style: TextStyle(
+                fontSize: 26,
+
+                color: Colors.deepPurple,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        title: Text('Quotes'),
+        centerTitle: true,
+        backgroundColor: Colors.orangeAccent,
+      ),
+      body: Column(
+// mainAxisAlignment: MainAxisAlignment.center,
+// crossAxisAlignment: CrossAxisAlignment.center,
+        children: quotes.map((quote) => quoteTemplate(quote)).toList(),
+      ),
+    );
+  }
+}

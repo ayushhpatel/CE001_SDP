438a9516d73aae78ca824b702cc5142f4aa25a00
ayushhpatel <84513508+ayushhpatel@users.noreply.github.com>
2022-09-26 20:41:35 +0530

    Update loading.dart

--- LAB10/tutorial2/loading.dart
+++ LAB10/tutorial2/loading.dart
@@ -2,38 +2,49 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart';
 import 'dart:convert';
 
-
 class Loading extends StatefulWidget {
-// const Loading({Key? key}) : super(key: key);
- @override
- State<Loading> createState() => _LoadingState();
+  const Loading({Key? key}) : super(key: key);
+
+  @override
+  State<Loading> createState() => _LoadingState();
 }
 
 class _LoadingState extends State<Loading> {
 
+  void getTime() async{
 
- void getData() async {
-   final response = await
-   get(Uri.parse('https://jsonplaceholder.typicode.com/albums/1'));
-   print(response.body); // it response in JSON form out put ...we need MAP format..
-// print(response.body.userId); // this will not work. because its not MAP format ..
-// TO CONVERT JSON TO MAP..WE NEED TO IMPORT convert package....
-   Map data = jsonDecode(response.body);
-   print(data);
-   print(data['title']);
- }
+    Response res=await get(Uri.parse('https://worldtimeapi.org/api/timezone/Europe/Amsterdam'));
+    Map tdata=jsonDecode(res.body);
+    print(tdata);
 
- @override
- void initState() {
-   super.initState();
-   getData();
- }
+    String dateTime=tdata['datetime'];
+    String offset=tdata['utc_offset'];
 
+    print(dateTime);
+    print(offset);
 
- @override
- Widget build(BuildContext context) {
-   return Scaffold(
-     body: Text('LOADING SCREEN'),
-   );
- }
+    DateTime currentTime=DateTime.parse(dateTime);
+    print(currentTime);
+
+    String offsetHours=offset.substring(1,3);
+    print(offsetHours);
+    String offsetMinutes=offset.substring(4,6);
+    print(offsetMinutes);
+
+    currentTime=currentTime.add(Duration(minutes: int.parse(offsetMinutes),hours: int.parse(offsetHours)));
+    print(currentTime);
+  }
+
+  @override
+  void initState(){
+    super.initState();
+    getTime();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Text('Loading Screen'),
+    );
+  }
 }

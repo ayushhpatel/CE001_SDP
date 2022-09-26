ebde1f2f1729bd505b35717c2f9ce6b9982a0342
ayushhpatel <84513508+ayushhpatel@users.noreply.github.com>
2022-09-26 10:09:19 +0530

    Update choose_location.dart

--- LAB10/tutorial1/code2/choose_location.dart
+++ LAB10/tutorial1/code2/choose_location.dart
@@ -1,36 +1,56 @@
 import 'package:flutter/material.dart';
 
 class ChooseLocation extends StatefulWidget {
-// const ChooseLocation({Key? key}) : super(key: key);
+  const ChooseLocation({Key? key}) : super(key: key);
+
   @override
   State<ChooseLocation> createState() => _ChooseLocationState();
 }
 
 class _ChooseLocationState extends State<ChooseLocation> {
-  int counter = 0;
+  int counter=0;
+
+  void getData() async{
+    String username=await Future.delayed(Duration(seconds: 4),(){
+      return 'University:DDU';
+    });
+
+    String bio=await Future.delayed(Duration(seconds: 2),(){
+      return 'DDU is one of the best University';
+    });
+
+    print('$username => $bio');
+  }
+
+  @override
+  void initState(){
+    super.initState();
+    print('Init state function run in choose location');
+    print('Before getdata() is called');
+    getData();
+    print('After getdata() is called');
+  }
+
+
   @override
   Widget build(BuildContext context) {
-// print('BUILD FUNCTION RUN IN CHOOSE LOCATION...');
+    // print("Build function run in choose location..");
     return Scaffold(
-      backgroundColor: Colors.blueGrey,
-// when we come/route from different screen IN APPBAR FLUTTER WILL DEFAULTLY
-// PLACED "BACK ARROW <- .....
+      backgroundColor: Colors.redAccent[200],
       appBar: AppBar(
-        backgroundColor: Colors.lightGreen,
-        title: Text('CHOOSE LOCATION'),
+        backgroundColor: Colors.pinkAccent,
+        title: Text('Choose Location'),
         centerTitle: true,
         elevation: 0,
       ),
-/*
-body: ElevatedButton(
-onPressed: () {
-setState(() {
-counter += 1;
-});
-},
-child: Text('COUNTER IS : $counter'),
-),
-*/
+      // body: ElevatedButton(
+      //   onPressed: (){
+      //     setState((){
+      //       counter+=1;
+      //     });
+      //   },
+      //   child: Text('counter is:$counter'),
+      // ),
     );
   }
 }
